89e11a7072792601cba1b8443b58dd998508c480
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-07-29 17:11:03 +0300

    SectionHeader componenti oluşturarak diğer parent componentlere -books contact us gibi- prop gönderme işlemleri yapıldı

---
+++ frontEnd/src/components/SectionHeader.vue
@@ -0,0 +1,28 @@
+<template>
+       <div class="text-center mb-5 mt-3">
+            <h1 class="display-4 font-weight-bolder">{{ title }}</h1>
+            <p class="lead">{{ text }}</p>
+        </div>
+</template>
+
+<script>
+    export default {
+        //hata ayıklamada derleyici tarafında hangi componenetin hata verdiğini anlamak için kullanılır.
+        name : 'SectionHeader',
+        // 
+        props : {
+            title : {
+                type : String,
+                required : true
+            },
+            text : {
+                type : String,
+                required : true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- frontEnd/src/views/BooksView.vue
+++ frontEnd/src/views/BooksView.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-        BooksView Page
+  <section>
+    <div class="container">
+        <!-- <SectionHeader title = "Books" text="Books Text"/> -->
+         <!-- Burada datada tanımlanan sectiontitle ı SectionHeader a prop olarak gönderebilmek için v-bind:title olarak kullanırız.
+            Bu şekilde SectionHeader componentine title propu olarak sectionTitle ı göndermiş oluruz. :title olarak da kullanılır.-->
+        <!-- <SectionHeader :title="sectionTitle" text="Books Text"/> -->
+        <SectionHeader title="Books" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!"/>
+        
     </div>
+  </section>
 </template>
 
 <script>
+import SectionHeader from '@/components/SectionHeader.vue'
     export default {
         //hata ayıklamada derleyici tarafında hangi componenetin hata verdiğini anlamak için kullanılır.
-        name : 'BooksView'
+        name : 'BooksView',
+        components: {
+            SectionHeader
+        },
+        data() {
+            return {
+                // sectionTitle : "Kitaplar",
+
+            }
+        },
     }
 </script>
 
