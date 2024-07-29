12d079ee23482b6ee3ab5104f9c19948e7e3560f
Betül Bircan <betul.bircan_1996@hotmail.com>
2024-07-29 17:29:18 +0300

    2:57 devam edecek

--- frontEnd/src/views/BooksView.vue
+++ frontEnd/src/views/BooksView.vue
@@ -6,6 +6,16 @@
             Bu şekilde SectionHeader componentine title propu olarak sectionTitle ı göndermiş oluruz. :title olarak da kullanılır.-->
         <!-- <SectionHeader :title="sectionTitle" text="Books Text"/> -->
         <SectionHeader title="Books" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!"/>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="card-border-0 shadow position-relative">
+                    <img src="../../template/images/b1.jpg" class="card-img-top">
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
   </section>
