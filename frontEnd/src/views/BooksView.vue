75d91dd98a2fca00b2bc3031b38169774ecc2834
Betul Bircan <betul.bircan@fairteknoloji.com.tr>
2024-08-05 17:40:39 +0300

    dinamik veriler oluşması için db.js dosyası oluşturuldu. v-for döngüsü kullanıldı. kitaplar listelendi

--- frontEnd/src/views/BooksView.vue
+++ frontEnd/src/views/BooksView.vue
@@ -1,43 +1,41 @@
 <template>
-  <section>
-    <div class="container">
-        <!-- <SectionHeader title = "Books" text="Books Text"/> -->
-         <!-- Burada datada tanımlanan sectiontitle ı SectionHeader a prop olarak gönderebilmek için v-bind:title olarak kullanırız.
+    <section>
+        <div class="container">
+            <!-- <SectionHeader title = "Books" text="Books Text"/> -->
+            <!-- Burada datada tanımlanan sectiontitle ı SectionHeader a prop olarak gönderebilmek için v-bind:title olarak kullanırız.
             Bu şekilde SectionHeader componentine title propu olarak sectionTitle ı göndermiş oluruz. :title olarak da kullanılır.-->
-        <!-- <SectionHeader :title="sectionTitle" text="Books Text"/> -->
-        <SectionHeader title="Books" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!"/>
-        <div class="row">
-            <div class="col-sm-6 col-md-4 col-xl-3">
-                <div class="card-border-0 shadow position-relative">
-                    <img src="../../template/images/b1.jpg" class="card-img-top">
-                    <div class="card-body">
-                        
-                    </div>
-                </div>
-            </div>
+            <!-- <SectionHeader :title="sectionTitle" text="Books Text"/> -->
+            <SectionHeader title="Books"
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!" />
+            <BookList :books="books"/>
+
         </div>
-        
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
 import SectionHeader from '@/components/SectionHeader.vue'
-    export default {
-        //hata ayıklamada derleyici tarafında hangi componenetin hata verdiğini anlamak için kullanılır.
-        name : 'BooksView',
-        components: {
-            SectionHeader
-        },
-        data() {
-            return {
-                // sectionTitle : "Kitaplar",
+import BookList from '@/components/BookList.vue';
+import books from '@/db.js';
+export default {
+    //hata ayıklamada derleyici tarafında hangi componenetin hata verdiğini anlamak için kullanılır.
+    name: 'BooksView',
+    components: {
+        SectionHeader,
+        BookList
+    },
+    data() {
+        return {
+            // sectionTitle : "Kitaplar",
+            books: books
 
-            }
-        },
-    }
+        }
+    },
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.auth-box {
+    margin-top: -30px;
+}
 </style>
