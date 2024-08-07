3b3de061a429f26ee20ea088989c1d1efb18e9f7
Betul Bircan <betul.bircan@fairteknoloji.com.tr>
2024-08-07 17:12:02 +0300

    11.ders 5:01 devam edecek

--- frontEnd/src/components/Pagination.vue
+++ frontEnd/src/components/Pagination.vue
@@ -1,15 +1,15 @@
 <template>
     <nav aria-label="Page navigation example">
   <ul class="pagination justify-content-end">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
+    <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+      <a class="page-link" :click="handleClick" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
     <li class="page-item"><a class="page-link" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
+    <li class="page-item" :class="{ 'disabled' : currentPage === totalPages}">
       <a class="page-link" href="#" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
       </a>
