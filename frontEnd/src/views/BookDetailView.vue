29f6a5a6c1c88d19446fa47373acaeaa80d5ed9e
Betul Bircan <betul.bircan@fairteknoloji.com.tr>
2024-08-13 11:14:19 +0300

    Kitap detay sayfası oluşturuldu

--- frontEnd/src/views/BookDetailView.vue
+++ frontEnd/src/views/BookDetailView.vue
@@ -1,34 +1,146 @@
 <template>
-   <section>
-    <div class="container">
-        <SectionHeader title="Book Detail"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!" />
-        <button>Back</button>
-        <div class="row">
-            <div class="col-lg-6">
-                <v-img class="card-img-top" src="../../template/images/b_detail.jpg" alt="Book Image"></v-img>
-            </div>
-            <div class="col-lg-6">
-                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda reiciendis aliquam minima libero adipisci itaque, architecto sed accusantium ut eum!</p>
-                <div class="col-lg-6"></div>
-                <div class="col-lg-6"></div>
+    <section>
+        <div class="container">
+            <SectionHeader title="Book Detail"
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae cumque nisi laborum harum ipsam!" />
+            <button>Back</button>
+            <div class="row mb-4">
+                <div class="col-lg-6">
+                    <img class="card-img-top" src="../../template/images/b_detail.jpg" alt="Book Image"></img>
+                </div>
+                <div class="col-lg-6 details-wrapper">
+                    <p class="lead description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda reiciendis
+                        aliquam minima libero adipisci itaque, architecto sed accusantium ut eum!
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda reiciendis
+                        aliquam minima libero adipisci itaque, architecto sed accusantium ut eum!
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda reiciendis
+                        aliquam minima libero adipisci itaque, architecto sed accusantium ut eum!
+                    </p>
+                    <div class="mb-4">
+                        <div class="row border-bottom pb-2">
+                            <div class="col-lg-6"><strong>Page</strong></div>
+                            <div class="col-lg-6">278</div>
+                        </div>
+                        <div class="row border-bottom pb-2">
+                            <div class="col-lg-6"><strong>Category</strong></div>
+                            <div class="col-lg-6">Fiction</div>
+                        </div>
+                        <div class="row border-bottom pb-2">
+                            <div class="col-lg-6"><strong>Rating</strong></div>
+                            <div class="col-lg-6">7.8</div>
+                        </div>
+                        <div class="row border-bottom pb-2">
+                            <div class="col-lg-6"><strong>Upload Date</strong></div>
+                            <div class="col-lg-6">19 Jan 2019</div>
+                        </div>
+                    </div>
+                    
+                    <div class="comments-section">
+                       <h3 class="display-6 mb-2">Comments</h3>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                       <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, expedita.</p>
+                                <div class="d-flex justify-content-between">
+                                    <p class="fw-bold fst-italic">John Doe</p>
+                                    <div class="d-flex align-items-center">
+                                        <p>Upvote</p>
+                                        <p class="ps-2" ><strong>8</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+                </div>
             </div>
         </div>
-        
-    </div>
-   </section>
+    </section>
 </template>
 
 <script>
-    import SectionHeader from '@/components/SectionHeader.vue'
-    export default {
-        name : "BookDetailView",
-        components : {
-            SectionHeader
-        }
+import SectionHeader from '@/components/SectionHeader.vue'
+export default {
+    name: "BookDetailView",
+    components: {
+        SectionHeader
     }
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.details-wrapper {
+    max-height: 740px;
+    display: flex;
+    flex-direction: column;
+}
 
+.comments-section {
+    flex-grow:1;
+    overflow-y: auto;
+}
+
+.description {
+    min-height: 150px;
+    max-height : 250px;
+    overflow-y: auto;
+}
 </style>
